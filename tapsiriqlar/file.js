cd9ff77ca50a91b5f356f3491e6b8f236e9b67cf
zarifaahmed <40523991+zarifaahmed@users.noreply.github.com>
2019-06-02 15:39:17 +0400

    homework

---
+++ tapsiriqlar/file.js
@@ -0,0 +1,72 @@
+
+//1. 
+
+// var a=454455;
+// var b=a.toString();
+// var c=b.split("");
+// console.log(c.length);
+
+
+//2.
+//  var arr=[2,3,6,1,5]
+// var c=0;
+// function myFunc(){
+
+//  for(var index=0;index<arr.length;index++){
+//  c=arr[index]
+// if(0>c||c>10){
+// return 0;
+// }else{
+//     return 1;
+// }
+// }
+// }
+// console.log(myFunc())
+
+
+
+
+
+//3.
+
+
+// var arr=[45,44,75,15,55,98,55,7,6,8,55,79,19];
+// console.log(arr.reduce(function(a,b){return Math.max(a,b)}))
+
+
+
+
+//4.
+// var arr=[45,44,75,3,55,98,55,7,6,8,55,79,19];
+//  console.log(arr.reduce(function(a,b){return Math.min(a,b)}))
+
+
+
+//5.
+
+
+
+//6.
+// var arr=[45,44,75,15,55,98,55,7,6,8,55,79,19];
+//  var a=arr.sort(function(a,b) {return a-b});
+// console.log(a[a.length-1]+","+a[a.length-2])
+
+
+//7.
+// var a=[1,2,3,4,5,6,7,8,9,10]
+// var even=[];
+// var odd=[];
+
+// for(var index=0;index<=a.length-1;index++){
+// var k=a[index]
+// if(k%2==0){
+//     even.push(k)
+// }else{
+//     odd.push(k)
+// }
+// }
+
+// console.log("even  "+even)
+// console.log("odd  "+odd)
+
+
